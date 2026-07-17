61cb2cf795017b1753fa4fca801138486bd31839
marcos-ordo <ordonezmarcos935@gmail.com>
2026-07-16 22:19:38 -0300

    respuesta posible para parte 2 (muy lenta)

--- app/Day9/Theather.hs
+++ app/Day9/Theather.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# HLINT ignore "Use list comprehension" #-}
+
 module Day9.Theather where
-import Data.Ord
-import Data.List
 
 type Pos = (Int,Int) -- (Eje X, Eje Y)
 data Intervalo = I Int (Int -> Bool) -- (Eje Y / nivel) (Intervalo X)
@@ -12,55 +12,10 @@ getBiggestArea = biggestArea . parsePos
 
 getBiggestInsider :: String -> Int
 getBiggestInsider s = let ps = parsePos s
-                          is = makeIntervals $ makePairs $ orderPos ps
-                       in biggestInsider is ps
+                          f  = pertenece ps
+                       in biggestInsider2 (odd . f) ps
 
--- TESTS
-
-liveTest :: IO ()
-liveTest = do s <- readFile "./app/Day9/input.txt"
-              print $ maxPosWithSameY $ orderPos $ parsePos s
-    where
-        maxPosWithSameY :: [Pos] -> Int
-        maxPosWithSameY []     = 0
-        maxPosWithSameY (p:ps) = f p ps 1
-        f :: Pos -> [Pos] -> Int -> Int
-        f p []      n = n
-        f p (p':ps) n = if snd p == snd p' then max (n+1) (f p ps (n+1)) else max n (f p' ps 1)
-
-test :: Int
-test = getBiggestArea textExample
-
-test2 :: Pos -> [Pos] -> Bool
-test2 p ps = belongsIntervals p (makeIntervals $ makePairs $ orderPos ps)
-
-test3 :: [Pos] -> [(Pos, Pos)]
-test3 ps = g ps (makeIntervals $ makePairs $ orderPos ps)
-    where
-        g :: [Pos] -> [Intervalo] -> [(Pos, Pos)]
-        g ps is = filter (\(a,b) -> not (f is a b)) [(a,b) | a <- ps, b <- ps]
-        f :: [Intervalo] -> Pos -> Pos -> Bool
-        f is (x,y) (x',y')
-            | y == y' = True
-            | x == x' = True
-            | (y < y' && x < x') || (y > y' && x > x') = belongsIntervals (x',y) is && belongsIntervals (x,y') is
-            | (y < y' && x > x') || (y > y' && x < x') = belongsIntervals (x,y') is && belongsIntervals (x',y) is
-
-test4 :: [Pos] -> ((Pos, Pos), Int)
-test4 ps = g ps (makeIntervals $ makePairs $ orderPos ps)
-    where
-        g ps is = maximumBy (comparing snd) [ ((p,p'), rectangleArea p p')
-                                            | p <- ps
-                                            , p' <- ps
-                                            , f is p p']
-        f :: [Intervalo] -> Pos -> Pos -> Bool
-        f is (x,y) (x',y')
-            | y == y' = True
-            | x == x' = True
-            | (y < y' && x < x') || (y > y' && x > x') = belongsIntervals (x',y) is && belongsIntervals (x,y') is
-            | (y < y' && x > x') || (y > y' && x < x') = belongsIntervals (x,y') is && belongsIntervals (x',y) is
-        rectangleArea :: Pos -> Pos -> Int
-        rectangleArea (x,y) (x',y') = (max x x' - min x x' + 1) * (max y y' - min y y' + 1)
+-- EXAMPLES
 
 textExample :: String
 textExample = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3"
@@ -93,97 +48,6 @@ biggestArea ps = foldr (\p n -> maxArea p ps `max` n) 0 ps
         maxArea p = foldr (\p' n -> rectangleArea p p' `max` n) 1
         rectangleArea :: Pos -> Pos -> Int
         rectangleArea (x,y) (x',y') = (max x x' - min x x' + 1) * (max y y' - min y y' + 1)
-
-{- PARA la parte 2, Creo que tengo que crear el contorno de tiles verdes como un conjunto, que luego, con cualquier Pos (tiles rojas) puedo saber si está dentro o fuera del area permitida-}
-
-orderPos :: [Pos] -> [Pos]
-orderPos []     = []
-orderPos [p]    = [p]
-orderPos (p:ps) = (orderPos . smaller p) ps ++ [p] ++ (orderPos . larger p) ps
-    where
-        smaller :: Pos -> [Pos] -> [Pos]
-        smaller (_,y) = orderByX . filter (\(_,y') -> y' < y)
-        larger :: Pos -> [Pos] -> [Pos]
-        larger (_,y) = orderByX . filter (\(_,y') -> y' >= y)
-
-orderByX :: [Pos] -> [Pos]
-orderByX []     = []
-orderByX [p]    = [p]
-orderByX (p:ps) = (orderByX . smaller p) ps ++ [p] ++ (orderByX . larger p) ps
-    where
-        smaller :: Pos -> [Pos] -> [Pos]
-        smaller (x,_) = filter (\(x',_) -> x' < x)
-        larger :: Pos -> [Pos] -> [Pos]
-        larger (x,_) = filter (\(x',_) -> x' >= x)
-
-makePairs :: [Pos] -> [(Pos,Pos)]
-makePairs = g . f . makeOffset
-    where
-        g :: [(Pos,Pos)] -> [(Pos,Pos)]
-        g []        = []
-        g [p]       = [p]
-        g (p:p':ps) = insiders p p' ++ g (p':ps)
-        f :: [(Pos,Pos,Int)] -> [(Pos,Pos)]
-        f []            = []
-        f ((p,p',n):ps) = if n > 0
-                          then (p,applyOffset p' n): f ps
-                          else (applyOffset p n,p'): f ps
-        applyOffset :: Pos -> Int -> Pos
-        applyOffset (x,y) n = (x+n,y)
-
-insiders :: (Pos,Pos) -> (Pos,Pos) -> [(Pos,Pos)]
-insiders (p,p') (pr,pr') = (p,p') : f (1 + snd p) (snd pr) (p,p') (pr,pr')
-    where
-        f y y' (p,p') (pr,pr')
-            | y == y' = []
-            | y <  y' = if fst p == fst pr
-                        then ((fst p, y),(min (fst p') (fst pr'), y)) : f (y+1) y' (p,p') (pr,pr')
-                        else ((max (fst p) (fst pr), y),(fst p', y)) : f (y+1) y' (p,p') (pr,pr')
-
-makeOffset :: [Pos] -> [(Pos,Pos,Int)]
-makeOffset []        = []
-makeOffset [p, p']   = [(p,p',0)]
-makeOffset (p:p':ps) = (f . makeOffset) ps p p'
-    where
-        f :: [(Pos,Pos,Int)] -> Pos -> Pos -> [(Pos,Pos,Int)]
-        f []                 _ _  = error "No debería haber pasado ..."
-        f ps'@((pr,pr',n):_) p p'
-            | n <= 0 && fst p == fst pr' = (p,p',offset pr p + n) : ps'
-            | n > 0  && fst p == fst pr' = (p,p',offset pr' p' + n) : ps'
-            | n >= 0 && fst p' == fst pr = (p,p',offset pr' p' + n) : ps'
-            | n < 0  && fst p' == fst pr = (p,p',offset pr p + n) : ps'
-            | fst p  == fst pr  = (p,p',offset pr' p' + n) : ps'
-            | fst p' == fst pr' = (p,p',offset pr p + n) : ps'
-            | otherwise = error $ "No debería haber pasado ... " ++ show ps' ++ " " ++ show p ++ " " ++ show p'
-        offset :: Pos -> Pos -> Int
-        offset (x,_) (x',_) = x - x'
-
-makeIntervals :: [(Pos,Pos)] -> [Intervalo]
-makeIntervals = map f
-    where
-        f :: (Pos,Pos) -> Intervalo
-        f ((x, y), (x', _)) = I y (\x'' -> x'' >= x && x'' <= x')
-
-belongsIntervals :: Pos -> [Intervalo] -> Bool
-belongsIntervals p = any (belongsIn p)
-    where
-        belongsIn :: Pos -> Intervalo -> Bool
-        belongsIn (x,y) (I y' f) = y == y' && f x
-
-biggestInsider :: [Intervalo] -> [Pos] -> Int
-biggestInsider is ps = foldr (\p n -> maxArea p is ps `max` n) 1 ps
-    where
-        maxArea :: Pos -> [Intervalo] -> [Pos] -> Int
-        maxArea p is = foldr (\p' n -> if f is p p' then rectangleArea p p' `max` n else n) 1
-        rectangleArea :: Pos -> Pos -> Int
-        rectangleArea (x,y) (x',y') = (max x x' - min x x' + 1) * (max y y' - min y y' + 1)
-        f :: [Intervalo] -> Pos -> Pos -> Bool
-        f is (x,y) (x',y')
-            | y == y' = True
-            | x == x' = True
-            | (y < y' && x < x') || (y > y' && x > x') = belongsIntervals (x',y) is && belongsIntervals (x,y') is
-            | (y < y' && x > x') || (y > y' && x < x') = belongsIntervals (x,y') is && belongsIntervals (x',y) is
-            | otherwise = False
 
 {-
  0123456789
@@ -225,3 +89,52 @@ biggestInsider is ps = foldr (\p n -> maxArea p is ps `max` n) 1 ps
 7.........+! + .
 8........... . .
 -}
+
+biggestInsider2 :: (Pos -> Bool) -> [Pos] -> Int
+biggestInsider2 f ps = foldr (\p n -> maxArea p f ps `max` n) 1 ps
+    where
+        maxArea :: Pos -> (Pos -> Bool) -> [Pos] -> Int
+        maxArea p f = foldr (\p' n -> if checkPerimeter f p p' then rectangleArea p p' `max` n else n) 1
+        rectangleArea :: Pos -> Pos -> Int
+        rectangleArea (x,y) (x',y') = (max x x' - min x x' + 1) * (max y y' - min y y' + 1)
+
+checkPerimeter :: (Pos -> Bool) -> Pos -> Pos -> Bool
+checkPerimeter f (x,y) (x',y') = all f (g (min x x') (max x x') (min y y') (max y y'))
+    where
+        g :: Int -> Int -> Int -> Int -> [Pos]
+        g minX maxX minY maxY = weirdZip minY [minX..maxX] maxY [minX..maxX] minX [minY..maxY] maxX [minY..maxY]
+        
+weirdZip :: Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [Pos]
+weirdZip minY []       maxY []       minX []       maxX []       = []
+weirdZip minY []       maxY []       minX []       maxX (mY:mYs) = (maxX,mY) : weirdZip minY []  maxY []  minX []  maxX mYs
+weirdZip minY []       maxY []       minX (mY:mYs) maxX mys      = (minX,mY) : weirdZip minY []  maxY []  minX mYs maxX mys
+weirdZip minY []       maxY (mX:mXs) minX mYs      maxX mys      = (mX,maxY) : weirdZip minY []  maxY mXs minX mYs maxX mys
+weirdZip minY (mX:mXs) maxY mxs      minX mYs      maxX mys      = (mX,minY) : weirdZip minY mXs maxY mxs minX mYs maxX mys
+
+pertenece :: [Pos] -> Pos -> Int
+pertenece ps p = f p (head ps) ps 0
+    where
+        f :: Pos -> Pos -> [Pos] -> Int -> Int
+        f _  _  []  n = n
+        f pu po [p] n
+            | isBetween pu p po = 1
+            | pu == p           = 1
+            | isVertical   p po = n + cruzarRayo pu (p,po)
+            | otherwise         = n
+        f pu po (p:p':ps) n
+            | isBetween pu p p' = 1
+            | isAPoint  pu p p' = 1
+            | isVertical   p p' = f pu po (p':ps) (n + cruzarRayo pu (p,p'))
+            | otherwise         = f pu po (p':ps) n
+        isVertical :: Pos -> Pos -> Bool
+        isVertical (x,_) (x',_) = x == x'
+        isBetween :: Pos -> Pos -> Pos -> Bool
+        isBetween (xu,yu) (x,y) (x',y') = xu == x && yu > y && yu < y'
+        isAPoint :: Pos -> Pos -> Pos -> Bool
+        isAPoint pu p p' = pu == p || pu == p'
+
+cruzarRayo :: Pos -> (Pos,Pos) -> Int
+cruzarRayo (x,y) (p,p') = fromEnum $ fst p > x && y > apply min snd p p' && y < apply max snd p p'
+
+apply :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+apply f g x y = f (g x) (g y)
